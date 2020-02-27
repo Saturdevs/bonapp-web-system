323a1384542746974635534cc4956d27ec10b445
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2020-02-26 21:30:33 -0300

    Notificaciones. Incompleto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketIoService } from '../shared/services/socket-io.service';
 import { ToastService } from 'ng-uikit-pro-standard';
+import { SwPush } from '@angular/service-worker';
+import { NotificationService } from '../shared/services/notification.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  readonly VAPID_PUBLIC_KEY = "BFXG3OYw9d3Nsd-h8yKWxKI0ok4ISyjtIOpqRBOOIy_bkBnP6QbCEzaN9cu-ac3yJFaduuqFvcFWvXrj9FB-t3E";
   title: string = 'Web Bar';
   subtitle: string = 'by Los Pibes';
   myDate: Date;
 
   constructor(private _socketService: SocketIoService,
-    private toast: ToastService) { }
+    private toast: ToastService,
+    private swPush: SwPush,
+    private newsletterService: NotificationService) { }
 
   ngOnInit() {
     this.getTime();
@@ -35,4 +40,12 @@ export class AppComponent implements OnInit {
     }, 1000);
   };
 
+  subscribeToNotifications() {
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
