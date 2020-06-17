37cadc04c046eb343208fe67a6e6a5d338e224bd
imchiodo <imchiodo1@gmail.com>
2020-06-16 23:52:46 -0300

    QRGen

--- src/app/qrGenerator/qr-generator.component.ts
+++ src/app/qrGenerator/qr-generator.component.ts
@@ -3,6 +3,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ActivatedRoute } from '@angular/router';
 import { AuthenticationService, Table } from '../../shared';
 import { QrGeneratorService } from '../../shared/services/qr-generator.service';
+import { SettingsService } from '../../shared/services/settings.service';
 
 @Component({
   selector: 'app-qr-generator',
@@ -14,7 +15,8 @@ export class QrGeneratorComponent implements OnInit {
   constructor(private route: ActivatedRoute,
     private modalService: BsModalService,
     private _authenticationService: AuthenticationService,
-    private qrGeneratorService: QrGeneratorService) { }
+    private qrGeneratorService: QrGeneratorService,
+    private settingsService: SettingsService) { }
 
   pageTitle: string = "Generar QR";
   tables: Array<Table>;
@@ -53,15 +55,23 @@ export class QrGeneratorComponent implements OnInit {
 
   generateQR(){
     this.tablesToGenerateQR.forEach(table => {
-      let data = {
-        tableNumber: table.number,
-        restaurantId: "lorenEsMedioGato"
-      };
-      this.qrGeneratorService.generateQR(data)
-        .subscribe(result => {
-          if(this.confirmationPath === ''){
-            this.confirmationPath = result.path;
-          }
+      this.settingsService.getAll()
+        .subscribe(settings => {
+          let data = {
+            tableNumber: table.number,
+            restaurantId: settings[0].restaurantId
+          };
+          this.qrGeneratorService.generateQR(data)
+            .subscribe(result => {
+              console.log(result);
+              
+              let fileName = "Mesa"+table.number+"QR";
+              this.download(fileName,result.data)
+
+              if(this.confirmationPath === ''){
+                this.confirmationPath = result.path;
+              }
+            });
         });
     });
     this.modalRef = this.modalService.show(this.confirmationTemplate, { backdrop: true });
@@ -72,5 +82,19 @@ export class QrGeneratorComponent implements OnInit {
     this.modalRef = null;
     return true;
   }
+
+    download(filename, qrBase64) {
+
+      let element = document.createElement('a');
+      element.setAttribute('href', qrBase64);
+      element.setAttribute('download', filename);
+    
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    
+      element.click();
+    
+      document.body.removeChild(element);
+    }
 
 }
